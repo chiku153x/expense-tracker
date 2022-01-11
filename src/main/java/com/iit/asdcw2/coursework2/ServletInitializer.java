e0fb416ba91ff73d420e81f46546636812502783
Gayan Chinthaka <gchinthaka@gmail.com>
2022-01-11 10:14:22 +0530

    initial commit

---
+++ src/main/java/com/iit/asdcw2/coursework2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.iit.asdcw2.coursework2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Coursework2Application.class);
+	}
+
+}
