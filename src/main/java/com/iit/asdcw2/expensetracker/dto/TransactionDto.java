e63ee586924c052ad3bfffd35acf3d2f7b1c5647
Gayan Chinthaka <gchinthaka@gmail.com>
2022-01-14 19:24:33 +0530

    swagger added

---
+++ src/main/java/com/iit/asdcw2/expensetracker/dto/TransactionDto.java
@@ -0,0 +1,12 @@
+package com.iit.asdcw2.expensetracker.dto;
+
+import java.io.Serializable;
+
+import io.swagger.annotations.ApiModel;
+
+@ApiModel("Transaction")
+public class TransactionDto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
