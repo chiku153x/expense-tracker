bd49af6b8c7e3e317bc34a344736859ed8f820c7
Gayan Chinthaka <gchinthaka@gmail.com>
2022-01-15 08:43:26 +0530

    downgrade swagger and point to default path

--- src/main/java/com/iit/asdcw2/config/DataAccessConfig.java
+++ src/main/java/com/iit/asdcw2/config/DataAccessConfig.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
